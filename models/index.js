15e3c856c734925164b27629ae2b71143fb29881
Reinel Parrado <reinelparrado@gmail.com>
2021-09-03 09:28:45 -0500

    got rid of threads model and schema

--- models/index.js
+++ models/index.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose')
 const MainSchema = require('./mainInfo')
 const ReplySchema = require('./replyInfo')
-const ThreadSchema = require('./threads')
-// Set Up Models Here
 
 const Main = mongoose.model('mains', MainSchema)
 const Reply = mongoose.model('replies', ReplySchema)
-const Thread = mongoose.model('threads', ThreadSchema)
 
 module.exports = {
-  // Export Models Here
   Main,
-  Reply,
-  Thread
+  Reply
 }
