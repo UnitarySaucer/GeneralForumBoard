a8619b7cd04f4968be2f3df2da540a112673006d
Reinel Parrado <reinelparrado@gmail.com>
2021-09-02 19:30:08 -0500

    added routers

---
+++ routes/Routers.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+const controllers = require('../controllers/index')
+const router = Router()
+
+router.get('/mainposts', controllers.getAllMain)
+router.post('/post', controllers.createMain)
+router.delete('/delete/:id', controllers.deleteMain)
+
+router.get('/replies', controllers.getAllReplies)
+router.post('/reply', controllers.createReply)
+// router.post('/delete/:id', controllers.deleteReply)
+
+module.exports = router
