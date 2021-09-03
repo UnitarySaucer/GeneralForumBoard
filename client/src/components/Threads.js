9a284d5db01a74dbeeefcccde9edc52dcd23ffcc
Reinel Parrado <reinelparrado@gmail.com>
2021-09-03 09:28:24 -0500

    changed layout of threads component to truly be only the threads

--- client/src/components/Threads.js
+++ client/src/components/Threads.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import Card from 'react-bootstrap/Card'
+// import Card from 'react-bootstrap/Card'
 import Nav from 'react-bootstrap/Nav'
 import { BASE_URL } from '../globals'
 
@@ -16,20 +16,15 @@ function Threads() {
   }, [])
 
   return (
-    <div className="catCont">
-      <Card>
-        <Card.Header as="h3">Threads</Card.Header>
-        <div>
-          {threadMain.map((thread) => {
-            return (
-              <Nav.Link
-                className="topics"
-                href={`/${thread._id}`}
-              >{`${thread.title}`}</Nav.Link>
-            )
-          })}
-        </div>
-      </Card>
+    <div>
+      {threadMain.map((thread) => {
+        return (
+          <Nav.Link
+            className="topics"
+            href={`/${thread._id}`}
+          >{`${thread.title}`}</Nav.Link>
+        )
+      })}
     </div>
   )
 }
