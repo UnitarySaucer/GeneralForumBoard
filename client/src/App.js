32b32a2c83488375df7adf4bc223782b89bd863b
Reinel Parrado <reinelparrado@gmail.com>
2021-09-03 01:48:47 -0500

    created react routes to switch to new pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import logo from './logo.svg'
+import { Route, Switch } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/gaming" component={Home} />
+          <Route exact path="/software" component={Home} />
+          <Route exact path="/media" component={Home} />
+          <Route exact path="/food" component={Home} />
+        </Switch>
+      </main>
+    </div>
+  )
 }
 
 export default App
