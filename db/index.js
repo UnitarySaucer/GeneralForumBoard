e4f837a859572e3da8a9b7f75fbed204ee0c436f
Reinel Parrado <reinelparrado@gmail.com>
2021-09-02 19:32:15 -0500

    added and deleted certain files/folders not needed and slightly modified others

--- db/index.js
+++ db/index.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/exampleDatabase'
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb+srv://unitarysaucer:Forum123@forum.y1zs7.mongodb.net/Forum?retryWrites=true&w=majority'
 
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
