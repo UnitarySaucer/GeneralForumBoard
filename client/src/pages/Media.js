c6bad60296a79c99bee4fc84d50551a09599793d
Reinel Parrado <reinelparrado@gmail.com>
2021-09-03 01:48:01 -0500

    created files for different pages

---
+++ client/src/pages/Media.js
@@ -0,0 +1,15 @@
+import Categories from '../components/Categories'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="cont">
+        <Categories />
+      </div>
+    </div>
+  )
+}
+
+export default Home
