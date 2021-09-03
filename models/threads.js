c07f9f2b8d7c80fe127776d1ae77ac56d15cd681
Reinel Parrado <reinelparrado@gmail.com>
2021-09-03 04:43:13 -0500

    deleted threads schema

--- models/threads.js
+++
@@ -1,9 +0,0 @@
-const { Schema } = require('mongoose')
-
-const Threads = new Schema({
-  postName: { type: Schema.Types.ObjectId, ref: 'mains' },
-  createdTime: { type: Schema.Types.ObjectId, ref: 'mains' },
-  updatedTime: { type: Schema.Types.ObjectId, ref: 'replies' }
-})
-
-module.exports = Threads
