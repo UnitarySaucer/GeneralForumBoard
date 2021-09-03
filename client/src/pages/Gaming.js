08e249fba3f526a09e380179129926533e59d953
Reinel Parrado <reinelparrado@gmail.com>
2021-09-03 04:28:02 -0500

    added ThreadForm to gaming page

--- client/src/pages/Gaming.js
+++ client/src/pages/Gaming.js
@@ -2,12 +2,16 @@
 import Threads from '../components/Threads'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ThreadForm from '../components/ThreadForm'
 
 function Gaming() {
   return (
     <div className="App">
       <div className="cont">
         <Threads />
+      </div>
+      <div className="cont">
+        <ThreadForm />
       </div>
     </div>
   )
