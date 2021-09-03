e29cb21b9d8ba3daebafd90b8e627a6779501f18
Reinel Parrado <reinelparrado@gmail.com>
2021-09-03 09:26:48 -0500

    tried to make functional comment box

--- client/src/pages/Gaming.js
+++ client/src/pages/Gaming.js
@@ -1,17 +1,56 @@
-// import Categories from '../components/Categories'
 import Threads from '../components/Threads'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Card from 'react-bootstrap/Card'
 import ThreadForm from '../components/ThreadForm'
+import { useEffect, useState } from 'react'
 
 function Gaming() {
+  const [threads, setThreadMain] = useState([])
+  const [newThread, setNewThread] = useState({
+    title: '',
+    content: ''
+  })
+
+  useEffect(() => {
+    async function getThreads() {
+      const res = await axios.get(`${BASE_URL}/mainposts`)
+      setThreadMain(res.data)
+    }
+    getThreads()
+  }, [])
+
+  const addThread = (e) => {
+    e.preventDefault()
+    const currentThreads = threads
+    const addedThread = { ...newThread }
+    currentThreads.push(addedThread)
+    setThreadMain(currentThreads)
+    setNewThread({ title: '', content: '' })
+  }
+
+  const handleChange = (e) => {
+    setNewThread({ ...newThread, [e.target.title]: e.target.value })
+  }
+
   return (
     <div className="App">
       <div className="cont">
-        <Threads />
+        <div className="catCont">
+          <Card>
+            <Card.Header as="h3">Threads</Card.Header>
+            <Threads />
+          </Card>
+        </div>
       </div>
       <div className="cont">
-        <ThreadForm />
+        <ThreadForm
+          newThread={newThread}
+          handleChange={handleChange}
+          addThread={addThread}
+        />
       </div>
     </div>
   )
