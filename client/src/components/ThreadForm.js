285477aec1bcd70fd79da8a2b2ccbe2fe1bbb918
Reinel Parrado <reinelparrado@gmail.com>
2021-09-03 09:27:25 -0500

    pass down props to comment box

--- client/src/components/ThreadForm.js
+++ client/src/components/ThreadForm.js
@@ -1,23 +1,36 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-// import FloatingLabel from 'react-bootstrap/FloatingLabel'
 
-function ThreadForm() {
+function ThreadForm(props) {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addThread(e)
+  }
+
   return (
     <div className="catCont">
-      <Form>
-        <Form.Group>
-          <Form.Control type="text" placeholder="Enter Title" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Control
-            as="textarea"
-            placeholder="Content Here"
-            style={{ height: '20vh' }}
-          />
-        </Form.Group>
-        <Button type="submit">Submit</Button>
-      </Form>
+      <form onSubmit={handleSubmit}>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              placeholder="Enter Title"
+              value={props.newThread.title}
+              onChange={props.handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              as="textarea"
+              placeholder="Content Here"
+              style={{ height: '20vh' }}
+              value={props.newThread.content}
+              onChange={props.handleChange}
+            />
+          </Form.Group>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </form>
     </div>
   )
 }
